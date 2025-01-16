339d494de41869c03fee49cd2feb76aebd073943
Nishit kumar <nishitku123@gmail.com>
2025-01-17 02:33:29 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,10 +9,11 @@ const registerUser = asyncHandler( async(req, res) =>{
 
     // get user details from frontend
     const {fullName, email, username, password} = req.body
-    console.log("email: ", email);
-    console.log("username: ", username);
-    console.log("fullName: ", fullName);
-    console.log("password: ", password);
+    // console.log("email: ", email);
+    // console.log("username: ", username);
+    // console.log("fullName: ", fullName);
+    // console.log("password: ", password);
+    console.log(req.body)
     
     // validation - not empty filed
     if ([fullName, email, username, password].some((field)=> field?.trim() === "")){
@@ -20,7 +21,7 @@ const registerUser = asyncHandler( async(req, res) =>{
     }
 
     // check if user already exist: username and email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     if(existedUser){
@@ -29,7 +30,11 @@ const registerUser = asyncHandler( async(req, res) =>{
 
     // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required");
     }
